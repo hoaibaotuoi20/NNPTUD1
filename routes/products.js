d0793dc18ebb35ce07c144bd41ba137e44d433ed
ngocnhat <bestichigobb@gmail.com>
2026-03-07 10:20:56 +0700

    Update products.js with MongoDB CRUD operations

--- routes/products.js
+++ routes/products.js
@@ -1,80 +1,115 @@
 const express = require('express')
 let router = express.Router()
-let { GenID, GetCateByID } = require('../utils/IDHandler')
 let slugify = require('slugify')
-let {dataProducts,dataCategories} = require('../utils/data')
+let productSchema = require('../schemas/products');//DBset/DBContext
 
-router.get('/', (req, res) => {
-    let result = dataProducts.filter(
-        function (e) {
-            return !e.isDeleted
+router.get('/:id', async (req, res) => {//req.params
+    try {
+        let dataProduct = await productSchema.findOne({
+            isDeleted: false,
+            _id: req.params.id
+        });
+        if (!dataProduct) {
+            res.status(404).send(
+                { message: "ID NOT FOUND" }
+            )
+        } else {
+            res.send(dataProduct)
         }
-    )
-    res.send(result)
-})
-router.get('/:id', (req, res) => {//req.params
-    let result = dataProducts.filter(
-        function (e) {
-            return e.id == req.params.id && !e.isDeleted;
-        }
-    )
-    res.send(result)
+    } catch (error) {
+        res.status(404).send(
+            { message: "something went wrong" }
+        )
+    }
 })
 
-router.post('/', (req, res) => {
-    let newProducts = {
-        id: GenID(dataProducts),
-        title: req.body.title,
-        slug: slugify(req.body.title, {
+router.get('/', async (req, res) => {//req.params
+    try {
+        let dataProducts = await productSchema.find({
+            isDeleted: false
+        });
+        res.send(dataProducts)
+    } catch (error) {
+        res.status(500).send(
+            { message: "something went wrong" }
+        )
+    }
+})
+
+router.post('/', async function (req, res, next) {
+    try {
+        let slug = slugify(req.body.title, {
             replacement: '-',
             lower: false,
             remove: undefined,
-        }),
-        description: req.body.description,
-        category: GetCateByID(req.body.category, dataCategories),
-        images: req.body.images,
-        creationAt: new Date(Date.now()),
-        updatedAt: new Date(Date.now())
-    }
-    dataProducts.push(newProducts);
-    res.send(newProducts)
-})
-router.put('/:id', (req, res) => {
-    let getProduct = dataProducts.filter(
-        function (e) {
-            return e.id == req.params.id && !e.isDeleted
-        }
-    )
-    if (getProduct.length > 0) {
-        getProduct = getProduct[0];
-        let keys = Object.keys(req.body);
-        for (const key of keys) {
-            if (getProduct[key]) {
-                getProduct[key] = req.body[key];
-            }
-        }
-        getProduct.updatedAt = new Date(Date.now());
-        res.status(200).send(getProduct)
-    } else {
-        res.status(404).send("id not found")
-    }
+        });
 
-})
-router.delete('/:id', (req, res) => {
-    let getProduct = dataProducts.filter(
-        function (e) {
-            return e.id == req.params.id && !e.isDeleted
-        }
-    )
-    if (getProduct.length > 0) {
-        getProduct = getProduct[0];
-        getProduct.isDeleted = true;
-        getProduct.updatedAt = new Date(Date.now());
-        res.status(200).send(getProduct)
-    } else {
-        res.status(404).send("id not found")
-    }
+        // Remove undefined fields to fallback to Mongoose defaults if needed
+        let productData = {
+            title: req.body.title,
+            slug: slug
+        };
+        if (req.body.price !== undefined) productData.price = req.body.price;
+        if (req.body.description !== undefined) productData.description = req.body.description;
+        if (req.body.images !== undefined) productData.images = req.body.images;
+        if (req.body.category !== undefined) productData.category = req.body.category;
 
+        let newItem = new productSchema(productData);
+        await newItem.save();
+        res.send(newItem)
+    } catch (error) {
+        res.status(400).send({ message: error.message })
+    }
+})
+
+router.put('/:id', async function (req, res, next) {
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title, {
+                replacement: '-',
+                lower: false,
+                remove: undefined,
+            });
+        }
+        let getItem = await productSchema.findByIdAndUpdate(
+            req.params.id, req.body, {
+            new: true
+        });
+        if (getItem) {
+            res.send(getItem)
+        } else {
+            res.status(404).send({
+                message: "ID NOT FOUND"
+            })
+        }
+    } catch (error) {
+        res.status(404).send(
+            { message: error.message }
+        )
+    }
+})
+
+router.delete('/:id', async function (req, res, next) {
+    try {
+        let getItem = await productSchema.findOne({
+            isDeleted: false,
+            _id: req.params.id
+        });
+        if (!getItem) {
+            res.status(404).send(
+                { message: "ID NOT FOUND" }
+            )
+        } else {
+            getItem.isDeleted = true
+            await getItem.save();
+            res.send(getItem)
+        }
+
+    } catch (error) {
+        res.status(404).send(
+            { message: error.message }
+        )
+    }
 })
 
 module.exports = router;
